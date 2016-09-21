456b19b1e89704ccf2a72e7773ccf94e729536b4
Tyoma77 <tyoma77@ya.ru>
2016-09-22 01:08:44 +0300

    Добавлена реализация инициализации 2 игроков

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,29 @@
 /**
- * Created by artem on 20.09.2016.
+ * Created by Artem on 20.09.2016.
+ * Главный класс для запуска игры
  */
 public class Game {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the tic tac toe game");
+
+        System.out.println("Enter Player1 name");
+        Player p1 = new Player();
+        p1.playerInitialize();
+
+        System.out.println("Enter Player2 name");
+        Player p2 = new Player();
+        p2.playerInitialize();
+
+        Field field = new Field();
+        field.fieldInitialize();
+        field.fieldPrint();
+
+//        while (true) {
+//            p1.playerTurn();
+//            p1.playerWin();
+//            p2.playerTurn();
+//            p1.playerWin();
+//        }
+    }
+
 }
--- src/Player.java
+++ src/Player.java
@@ -4,17 +4,57 @@ import java.io.InputStreamReader;
 
 /**
  * Created by artem on 20.09.2016.
- * игрок 1
+ * Класс игрока
  */
 public class Player {
-    public void playerInitialize(){
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            String name = reader.readLine();
-        } catch (Exception e) {
-            System.out.println("!!!Error!!!");
-            System.out.println("Enter ");
-        }
+    String name;
+    static int count = 0;
+
+    public static int getCount() {
+        return count;
     }
 
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public Player() {
+
+    }
+
+    public void playerInitialize() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            name = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while (true) {
+            if (name.isEmpty()) {
+                System.out.println("Error");
+                System.out.println("Enter your name again");
+                try {
+                    name = reader.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else
+                break;
+        }
+        if (getCount() == 0)
+            System.out.println("Thank you " + name + ". You play for \"0\"");
+        else
+            System.out.println("Thank you " + name + ". You play for \"X\"");
+        System.out.println();
+        setCount(1);
+    }
+
+    public void playerTurn() {
+        int count = 1;
+        System.out.println("");
+    }
+
+    public void playerWin() {
+
+    }
 }
